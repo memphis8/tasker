f58b60c4e3cc8579f900aa2eef44ce3af9795f1b
memphis8 <uplmatches@gmail.com>
2019-09-06 08:37:03 +0300

    Validation added

--- src/main/java/com/java/step/demo/controller/WelcomePageController.java
+++ src/main/java/com/java/step/demo/controller/WelcomePageController.java
@@ -2,11 +2,30 @@ package com.java.step.demo.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @AllArgsConstructor
-@RequestMapping("/")
 public class WelcomePageController {
+
+    @GetMapping({"/","home"})
+    public String getHome(){
+        return "home"; // need view "home"(or another name)
+    }
+
+    @GetMapping("/login")
+    public String getLogin(Model model, String error, String logout) {
+        if (error != null)
+            model.addAttribute("error", "Your username and password is invalid.");
+
+        if (logout != null)
+            model.addAttribute("message", "You have been logged out successfully.");
+
+        return "login";
+    }
+
 
 }
--- src/main/java/com/java/step/demo/entity/User.java
+++ src/main/java/com/java/step/demo/entity/User.java
@@ -5,6 +5,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.util.Set;
 
 @Data
@@ -17,8 +20,12 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @NotNull
+    @Size(min=5,max = 20, message="Name must be at least 5 characters long and no more than 20")
     private String login;
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{8,}$",message = "Incorrect password,read policy" )
     private String password;
+    @Pattern(regexp = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$",message = "Incorrect email" )
     private String email;
 
     @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
@@ -26,7 +33,18 @@ public class User {
     @Enumerated(EnumType.STRING)
     private Set<Role> roles;
 
+    /*
+    * The password policy is:
 
+At least 8 chars
+
+Contains at least one digit
+
+Contains at least one lower alpha char and one upper alpha char
+
+Contains at least one char within a set of special chars (@#%$^ etc.)
+
+Does not contain space, tab, etc.*/
 
 
 }
