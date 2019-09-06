a507651b1a5721f42d59583dbccddde4310c2edd
memphis8 <uplmatches@gmail.com>
2019-09-06 09:41:22 +0300

    Validation added

--- src/main/java/com/java/step/demo/entity/User.java
+++ src/main/java/com/java/step/demo/entity/User.java
@@ -33,18 +33,6 @@ public class User {
     @Enumerated(EnumType.STRING)
     private Set<Role> roles;
 
-    /*
-    * The password policy is:
-
-At least 8 chars
-
-Contains at least one digit
-
-Contains at least one lower alpha char and one upper alpha char
-
-Contains at least one char within a set of special chars (@#%$^ etc.)
-
-Does not contain space, tab, etc.*/
 
 
 }
