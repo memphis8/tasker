41da6fc51a3e8b75792836df3578f6f66b2c74de
Dima <melnik.dimoss@gmail.com>
2019-09-09 15:16:22 +0300

    Merge branch 'dima' of https://github.com/memphis8/tasker into dima
    # Conflicts:
    #	src/main/java/com/java/step/demo/controller/TaskController.java

--- src/main/java/com/java/step/demo/config/MvcConfig.java
+++ src/main/java/com/java/step/demo/config/MvcConfig.java
@@ -1,6 +1,8 @@
 package com.java.step.demo.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,6 +11,12 @@ public class MvcConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
     }
 
 }
